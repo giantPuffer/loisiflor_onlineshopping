7862c855930eed78eaf2c2545a625a89a939bc45
vatan <vatan@LO-DESK-CO45>
2018-07-02 17:05:33 +0200

    My project Setup Completed Successfully

---
+++ shoppingLoisiflorBackend/src/main/java/net/loisiflor/shoppingLoisiflorBackend/App.java
@@ -0,0 +1,13 @@
+package net.loisiflor.shoppingLoisiflorBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
